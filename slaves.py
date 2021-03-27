7a25877a383181ca222f2bf68bf41770b090d7f7
Вячеслав (Vyacheslav) <48851403+SlavatarGit@users.noreply.github.com>
2021-03-27 13:18:26 +0900

    added getSlaves by dungeonmaster

--- slaves.py
+++ slaves.py
@@ -6,6 +6,7 @@ buy_url = "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave" # URL 
 job_url = "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/jobSlave" # URL 4 job
 user_url = "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/user?id=" # URL 4 user
 fetter_url = "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter" # URL 4 fetter
+slaves_url = "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/slaveList?id=" # URL 4 slaves
 
 headers = {
     "accept": "application/json, text/plain, */*",
@@ -26,13 +27,14 @@ headers = {
 
 # Slave class & function
 class Slave:
-    def __init__(self, id):
-        r = requests.get(user_url + str(id), headers=headers)
-        if r.status_code == 200:
-            json = r.json()
-        else:
-            print(f"Oops! Error code {r.status_code}, {r.text}")
-            return
+    def __init__(self, id = 1, json = None):
+        if json == None:
+            r = requests.get(user_url + str(id), headers=headers)
+            if r.status_code == 200:
+                json = r.json()
+            else:
+                print(f"Oops! Error code {r.status_code}, {r.text}")
+                return
 
         self.item_type = json["item_type"]
         self.id = json["id"]
@@ -69,22 +71,56 @@ class Slave:
     def buyFetter(self):
         requests.post(fetter_url, headers=headers, data=json.dumps({"slave_id": self.id}))
 
+    def getSlaves(self):
+        r = requests.get(slaves_url + str(self.id), headers=headers)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return None
+
 
 # Example
 import random
 import time
 if __name__ == "__main__":
     while True:
+        """ Покупает рандомных рабов от 30к и до 150к
         slave = Slave(random.randint(1, 646516091))
         try:
             if slave.price > 30000 and slave.price < 150000 and not slave.fetter_to:
                 print(f"Finded slave {slave.id} cost {slave.price}")
                 slave.buy()
                 slave.setJob()
-                slave.buyFetter()
+                #slave.buyFetter()
                     
             else:
                 print(f"Slave {slave.id} cost {slave.price}; is fetter: {slave.fetter_to}, aborted")
         except:
             continue
+        """
+
+        """ Рейдит одного пользователя, скупая всех рабов от 30к до 300к
+        dungeonmaster = Slave(173217382)
+        dungeonmaster_slaves = dungeonmaster.getSlaves()
+        for i in dungeonmaster_slaves["slaves"]:
+            slave = Slave(json=i)
+            if slave.price > 30000 and slave.price < 300000 and not slave.fetter_to:
+                print(f"Finded slave {slave.id} cost {slave.price}")
+                slave.buy()
+                slave.setJob()
+                time.sleep(2.5)
+            else:
+                print(f"Slave {slave.id} cost {slave.price}; is fetter: {slave.fetter_to}, aborted")
+        """
         
+        """Устанавливает работы для всех рабов в вашем владении
+        dungeonmaster = Slave(411715303) # Твой айди тут
+        dungeonmaster_slave = dungeonmaster.getSlaves()
+        for i in dungeonmaster_slave["slaves"]:
+            slave = Slave(json=i)
+            slave.setJob("🦾 github.com/SlavatarGit")
+            if not slave.fetter_to: 
+                slave.buyFetter()
+
+            time.sleep(2.5)
+        """
